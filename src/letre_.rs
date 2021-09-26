649b32599b1718d53196689586e608a5618cf02b
Syed Huzaifa <rustingwork@gmail.com>
2021-09-26 19:18:08 +0500

    Software Updated Successfully

--- src/letre_.rs
+++ src/letre_.rs
@@ -1,46 +1,46 @@
-//////////////////////////////// Provides a simple email builder and several transports.
-//////////////////////////////// This mailer contains the available transports for your emails.
-//////////////////////////////// Lettre_email provides a simple email builder.
-use lettre::{Message, SmtpTransport, Transport};
-use lettre::transport::smtp::authentication::Credentials;
-//////////////////////////////// Message -> Email message which can be formatted.
-//////////////////////////////// StmpTransport -> Sends emails using the SMTP protocol.
-//////////////////////////////// Transport -> Represents an Email transport.
+// //////////////////////////////// Provides a simple email builder and several transports.
+// //////////////////////////////// This mailer contains the available transports for your emails.
+// //////////////////////////////// Lettre_email provides a simple email builder.
+// use lettre::{Message, SmtpTransport, Transport};
+// use lettre::transport::smtp::authentication::Credentials;
+// //////////////////////////////// Message -> Email message which can be formatted.
+// //////////////////////////////// StmpTransport -> Sends emails using the SMTP protocol.
+// //////////////////////////////// Transport -> Represents an Email transport.
 
-pub fn new(to: String , subject : String , body : String) {
+// pub fn new(to: String , subject : String , body : String) {
 
-    let email = Message::builder()
-        .from("rustingwork@gmail.com".parse().unwrap())
-        .to(to.parse().unwrap())
-        .subject(subject)
-        .body(body)
-        .unwrap();
+//     let email = Message::builder()
+//         .from("rustingwork@gmail.com".parse().unwrap())
+//         .to(to.parse().unwrap())
+//         .subject(subject)
+//         .body(body)
+//         .unwrap();
 
-        let creds = Credentials::new("rustingwork@gmail.com".to_string(), "syed@github2023".to_string());
+//         let creds = Credentials::new("rustingwork@gmail.com".to_string(), "syed@github2023".to_string());
 
-// Open a remote connection to gmail
-    let mailer = SmtpTransport::relay("smtp.gmail.com")
-        .unwrap()
-        .credentials(creds)
-        .build();
-    // Open a local connection on port 25
-    //let mailer = SmtpTransport::unencrypted_localhost();
+// // Open a remote connection to gmail
+//     let mailer = SmtpTransport::relay("smtp.gmail.com")
+//         .unwrap()
+//         .credentials(creds)
+//         .build();
+//     // Open a local connection on port 25
+//     //let mailer = SmtpTransport::unencrypted_localhost();
 
-    // Send the email
+//     // Send the email
 
-    match mailer.send(&email) {
-        Ok(i) => println!("{:?}",i),
-        Err(e) => println!("Could not send emails: {:?}", e),
-    }
-                            //RECEIVING ERROR:
-// Sep 19 16:49:53.957 ERROR r2d2: Connection error: Could not connect
-// Sep 19 16:49:58.443 ERROR r2d2: Connection error: Could not connect
-// Sep 19 16:50:03.297 ERROR r2d2: Connection error: Could not connect
-// Sep 19 16:50:08.961 ERROR r2d2: Connection error: Could not connect
-// Sep 19 16:50:16.260 ERROR r2d2: Connection error: Could not connect
+//     match mailer.send(&email) {
+//         Ok(i) => println!("{:?}",i),
+//         Err(e)  => println!("Could not send emails: {:?}", e),
+//     }
+//                             //RECEIVING ERROR:
+// // Sep 19 16:49:53.957 ERROR r2d2: Connection error: Could not connect
+// // Sep 19 16:49:58.443 ERROR r2d2: Connection error: Could not connect
+// // Sep 19 16:50:03.297 ERROR r2d2: Connection error: Could not connect
+// // Sep 19 16:50:08.961 ERROR r2d2: Connection error: Could not connect
+// // Sep 19 16:50:16.260 ERROR r2d2: Connection error: Could not connect
 
-                    // error return from above Match block:
-// Could not send emails: lettre::transport::smtp::Error { kind: Client, source: Error(Some("Connection error: Could not conn
-// ect")) }
-}
+//                     // error return from above Match block:
+// // Could not send emails: lettre::transport::smtp::Error { kind: Client, source: Error(Some("Connection error: Could not conn
+// // ect")) }
+// }
  
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,121 @@
-mod letre_;
-mod clap_;
-// use std::{env};
+use clap::{AppSettings, Clap};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
 
-fn main() {
-    let mail = clap_::new();
-    letre_::new(mail.email,mail.subject,mail.context);
+const USER_NAME: &'static str = "USER_NAME";
+const USER_EMAIL: &'static str = "USER_EMAIL";
+const PASSWORD: &'static str = "PASSWORD";
+const EMAIL_SERVER: &'static str = "smtp.gmail.com";
 
- 
+#[derive(Clap)]
+
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Email {
+    #[clap(short, long)]
+    name: String,
+    /// Email address to send email.
+    #[clap(short, long)]
+    email: String,
+
+    /// Subject Line for email
+    #[clap(short, long, default_value = "Yeh Email apko CLI sy musool hoi hain.")]
+    subject: String,
+
+    /// Content for Email body
+    #[clap(short, long, default_value = "Email jism khali hai.")]
+    body: String,
 }
+
+fn main() -> anyhow::Result<()> {
+    
+    // Sender Info from Env vars or secrets
+    let sender_name = dotenv::var(USER_NAME)?;
+    let sender_email = dotenv::var(USER_EMAIL)?;
+    let sender_passwd = dotenv::var(PASSWORD)?;
+    println!("{}",sender_email);
+
+    // Parse Cli opts
+    let args = Email::parse();
+
+    // Create email.
+    let email = Message::builder()
+        .from(
+            format!("{} <{}>", sender_name, sender_email)
+                .parse()
+                .unwrap(),
+        )
+        .to(format!("{} <{}>", &args.name, &args.email).parse().unwrap())
+        .subject(&args.subject)
+        .body(args.body.clone())
+        .unwrap();
+
+        // Open a remote connection to gmail
+//     let mailer = SmtpTransport::relay("smtp.gmail.com")
+//         .unwrap()
+//         .credentials(creds)
+//         .build();
+//     // Open a local connection on port 25
+//     //let mailer = SmtpTransport::unencrypted_localhost();
+
+    // Add credentials
+
+    let creds = Credentials::new(sender_email, sender_passwd);
+
+    // Open a remote connection to gmail
+    let mailer = SmtpTransport::relay(EMAIL_SERVER)
+        .unwrap()
+        .credentials(creds)
+        .build();
+
+    // Send the email
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => println!("Couldn't Send Email : {:#?}", e),
+    }
+
+    Ok(())
+}
+// pub fn new() -> Mail{
+
+// app name 
+//     let matches = App::new("Pegham")
+// // version
+//     .version("0.1.0")
+// //author
+//     .author("S_H_A_H")
+// // about the app
+//     .about("Ek baar azmaish Bar bar frmaish!")
+// // Email flag
+//     .arg(Arg::new("Email")
+//             .long("email") //
+//             .short('e')
+//             .value_name("EMAIL")
+//             .about("Write the email address")
+//             .takes_value(true)
+//         )
+// // Subject Flag
+//         .arg(Arg::new("Subject")
+//             .long("subject")
+//             .short('s')
+//             .value_name("SUBJECT")
+//             .about("Write the subject")
+//             .takes_value(true)
+//         )
+// // Content Flag
+//         .arg(Arg::new("Content")
+//             .long("content")
+//             .short('c')
+//             .value_name("CONTENT")
+//             .about("Write the content")
+//             .takes_value(true)
+//         )
+
+
+//         .get_matches();
+
+//     Mail{
+//         email:matches.value_of("Email").unwrap().to_string(),
+//         subject:matches.value_of("Subject").unwrap().to_string(),
+//         context:matches.value_of("Content").unwrap().to_string(),
+//     }
+//}
